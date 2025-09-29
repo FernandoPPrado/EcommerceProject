d830b0e35dedf45283114e151a541bec11c89f07
FernandoPPrado <fernandopradoifc@gmail.com>
2025-09-29 19:54:10 -0300

    CRIANDO ENTIDADE PURCHASESTATUS E PURCHASE

---
+++ src/main/java/com/example/demo/model/Purchase.java
@@ -0,0 +1,78 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Purchase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+
+    @ManyToOne
+    User user;
+
+    @ManyToOne
+    Product product;
+
+    @Column(nullable = false)
+    PurchaseStatus purchaseStatus;
+
+    LocalDateTime purchaseDate;
+
+    public Purchase() {
+        this.purchaseDate = LocalDateTime.now();
+        this.purchaseStatus = PurchaseStatus.PENDING;
+    }
+
+
+    public Purchase(User user, Product product, PurchaseStatus purchaseStatus, LocalDateTime purchaseDate) {
+        this.user = user;
+        this.product = product;
+        this.purchaseStatus = purchaseStatus;
+        this.purchaseDate = LocalDateTime.now();
+    }
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public PurchaseStatus getPurchaseStatus() {
+        return purchaseStatus;
+    }
+
+    public void setPurchaseStatus(PurchaseStatus purchaseStatus) {
+        this.purchaseStatus = purchaseStatus;
+    }
+
+    public LocalDateTime getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setPurchaseDate(LocalDateTime purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+}
